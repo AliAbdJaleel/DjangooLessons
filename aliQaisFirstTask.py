5e2c80aa2c9014ba7dacc148e79ff02ee9b79eb0
AliAbdJaleel <aliabdjaleel85@gmail.com>
2023-08-23 15:20:46 +0300

    remove all code from aliQaisFirstTask and add print hello world

--- aliQaisFirstTask.py
+++ aliQaisFirstTask.py
@@ -1,9 +1,1 @@
-print("Hello boys and girls")
-<<<<<<< HEAD
-print("Hello every one")
-=======
-print("Hello evry one")
-sum = 1+1
-print(sum)
->>>>>>> df522ff96c8d3e82a473b7f23b29dc3751bbad97
-print('new brsnch1')
+print("Hello world")
